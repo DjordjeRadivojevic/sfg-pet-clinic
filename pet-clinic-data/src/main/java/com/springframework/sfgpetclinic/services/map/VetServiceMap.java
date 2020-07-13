6a08217a711eb4a18eb8d93b78f3666a82bdfd08
djordje_radivojevic_eng <djordje.radivojevic.eng@gmail.com>
2020-07-13 10:39:03 +0200

    updatd profiles for JPA service, tested active profiles. closes #48

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -4,11 +4,13 @@ import com.springframework.sfgpetclinic.model.Vet;
 import com.springframework.sfgpetclinic.model.VetSpeciality;
 import com.springframework.sfgpetclinic.services.VetService;
 import com.springframework.sfgpetclinic.services.VetSpecialityService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService{
 
     private final VetSpecialityService specialityService;
--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/VetSpecialityServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/VetSpecialityServiceMap.java
@@ -2,11 +2,13 @@ package com.springframework.sfgpetclinic.services.map;
 
 import com.springframework.sfgpetclinic.model.VetSpeciality;
 import com.springframework.sfgpetclinic.services.VetSpecialityService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class VetSpecialityServiceMap extends AbstractMapService<VetSpeciality,Long> implements VetSpecialityService {
     @Override
     public Set<VetSpeciality> findAll() {
