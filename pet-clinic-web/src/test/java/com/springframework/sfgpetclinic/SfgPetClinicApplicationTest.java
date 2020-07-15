8203208b44dda20b305ac458871933c0e12af97d
djordje_radivojevic_eng <djordje.radivojevic.eng@gmail.com>
2020-07-15 14:39:37 +0200

    added tests. closes #49

--- pet-clinic-web/src/test/java/com/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
+++ pet-clinic-web/src/test/java/com/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -1,11 +1,9 @@
 package com.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
+//@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SfgPetClinicApplicationTest {
 
