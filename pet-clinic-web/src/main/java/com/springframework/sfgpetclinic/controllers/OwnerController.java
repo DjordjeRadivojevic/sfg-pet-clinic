95af409671643fd0870626b9c7bafea5213d4e21
djordje_radivojevic_eng <djordje.radivojevic.eng@gmail.com>
2020-10-22 09:09:11 +0200

    added owenr detail

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.springframework.sfgpetclinic.controllers;
 import com.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -17,7 +20,6 @@ public class OwnerController {
 
     @RequestMapping({"","/","/index","/index.html"})
     public String listOwners(Model model){
-
         model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
@@ -25,5 +27,12 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners(){
         return "notImplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId){
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
